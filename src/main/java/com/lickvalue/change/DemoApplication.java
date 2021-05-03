b55ea6a87e14e6dabe3277c6c606f8495fb87f42
Renzo <renzo@MacBook-Air-de-Renzo.local>
2021-05-03 01:28:54 -0300

    set value excepto coins by params

--- src/main/java/com/lickvalue/change/DemoApplication.java
+++ src/main/java/com/lickvalue/change/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -27,13 +28,18 @@ public class DemoApplication {
 		String setLong = args[0];
 		String setShort = args[1];
 		String percentage = args[2]; // Add params to increment value percentage
+		String exceptCoins = args[3];
+		String[] splitCoins = exceptCoins.split(",");
+		List<String> except = Arrays.stream(splitCoins).collect(Collectors.toList());
 
 		System.out.println("========================================");
-		System.out.println("Init Updating LickValue Pairs with params : " + percentage);
+		System.out.println(" Updating LickValue Pairs more percentage plus : " + percentage);
 		System.out.println(" Set Short & Long values with this values, long -> "
 				+ setLong + " and short -> " + setShort);
+		System.out.println(" set vwap Except this coins: " );
+		except.forEach(System.out::println);
 
-		getVarPairJson(percentage, setLong, setShort);
+		getVarPairJson(percentage, setLong, setShort, except);
 
 		System.out.println("======FINISH PAIRS VALUE UPDATE========");
 	}
@@ -45,7 +51,8 @@ public class DemoApplication {
 	}
 
 
-	private static VarPair getVarPairJson (String percentage, String setLong, String setShort) {
+	private static VarPair getVarPairJson (String percentage, String setLong,
+										   String setShort, List<String> except) {
 		Gson g = new Gson();
 		VarPair varPair = null;
 		try {
@@ -63,7 +70,7 @@ public class DemoApplication {
 			varPair = g.fromJson(reader , VarPair.class);
 
 			List<Coin> changeCoins = changeCoins(varPair.getCoins(), convertPairsToMap,
-					percentage, setLong, setShort);
+					percentage, setLong, setShort, except);
 
 			varPair.setCoins(changeCoins);
 
@@ -90,7 +97,8 @@ public class DemoApplication {
 
 
 	private static List<Coin> changeCoins(List<Coin> coins, Map<String, Pair> convertPairsToMap,
-										  String percentage, String setLong, String setShort) {
+										  String percentage, String setLong, String setShort,
+										  List<String> except) {
 		return coins.stream().map( coin -> {
 			Pair pair = convertPairsToMap.get(coin.getSymbol());
 			if (pair != null) {
@@ -98,8 +106,8 @@ public class DemoApplication {
 				String lickValue = String.valueOf((int)Math.round(pair.getAverage_usdt() + addPercentage ));
 				return new Coin(
 						coin.getSymbol(),
-						setLong,
-						setShort,
+						setLongMethod(coin.getSymbol(), except, coin.getLongoffset(), setLong),
+						setShortMethod(coin.getSymbol(), except, coin.getShortoffset(), setShort),
 						lickValue,
 						coin.getVar_enabled(),
 						coin.getVar_staticList(),
@@ -111,5 +119,21 @@ public class DemoApplication {
 				return null;
 			}
 		}).collect(Collectors.toList());
+	}
+
+	private static String setLongMethod(String symbol, List<String> except, String longOld,
+						   String longNew) {
+		if (except.contains(symbol)) {
+			return longOld;
+		}
+		return longNew;
+	}
+
+	private static String setShortMethod(String symbol, List<String> except, String shortOld,
+										String shortNew) {
+		if (except.contains(symbol)) {
+			return shortOld;
+		}
+		return shortNew;
 	}
 }
